71fbdc148017230bf864b0fe5a01a7f680e06a4d
Beau <Beau@broutec.nl>
2025-06-24 10:15:53 +0200

    Added scripts for scene loading and return to menu

--- ECM1VPb_LG2/Assets/Playtest/ScriptsPlaytest/PlaytestMenuManager.cs
+++ ECM1VPb_LG2/Assets/Playtest/ScriptsPlaytest/PlaytestMenuManager.cs
@@ -3,15 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class PlaytestMenuManager : MonoBehaviour
 {
-    public void StartPlaytest()
+    public void StartPlaytestV1()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void StartPlaytestV2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void StartPlaytestV3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void StartPlaytestV4()
+    {
+        SceneManager.LoadScene(4);
     }
 
     public void ExitGamePlaytest()
     {
         Application.Quit();
     }
-
-
 }
