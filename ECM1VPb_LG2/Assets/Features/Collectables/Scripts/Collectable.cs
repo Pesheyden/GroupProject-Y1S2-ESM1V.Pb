c074d5628ce26cd7fd0326ff4916ce01aa1f1ef8
Pesheyden <pasha.dan.dof@gmail.com>
2025-06-26 15:16:03 +0200

    Added scripts to level prefabs

--- ECM1VPb_LG2/Assets/Features/Collectables/Scripts/Collectable.cs
+++ ECM1VPb_LG2/Assets/Features/Collectables/Scripts/Collectable.cs
@@ -1,14 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using BSOAP.Variables;
-using FMODUnity;
 using UnityEngine;
-using UnityEngine.Networking.PlayerConnection;
 
 public class Collectable : MonoBehaviour
 {
-    
-    public GameObject CollectableGameObject;
     public string Parameter;
 
     private void OnTriggerEnter(Collider other)
